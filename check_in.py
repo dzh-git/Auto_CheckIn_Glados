bd993ed3ac90a2c28f60e3e705781dbaf18f7064
dzh-git <70357149+dzh-git@users.noreply.github.com>
2022-08-27 10:47:59 +0800

    Update check_in.py

--- check_in.py
+++ check_in.py
@@ -19,23 +19,19 @@ def start():
         'token': 'glados.network'
     }
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    # state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
     print(checkin)
-    print(state)
-
+    # with open("checkin.txt",'w') as fp:
+    #     fp.write(json.dumps(checkin.json(),indent=4))
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+        print(mess)
     else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        print("no message")
 
 def main_handler(event, context):
   return start()
 
 if __name__ == '__main__':
     start()
-
